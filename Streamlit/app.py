a15a58aaf3d7a6d3b3b1c880d448202fdfb5ce04
Mounish-R <rmounish83@gmail.com>
2026-03-16 19:33:23 +0530

    image update

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -6,7 +6,7 @@ st.header("Welcome to My Page")
 
 st.write("My Name is MOUNISH R")
 
-st.image("logo.jpg", caption="Sample Image")
+st.image("assest/logo.jpg", caption="Sample Image")
 
 name = st.text_input("Enter your name")
 
